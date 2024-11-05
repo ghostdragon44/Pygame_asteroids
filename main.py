877792332c73de18ab2f02a743f251f48feda48a
ghostdragon44 <kagrasse98@gmail.com>
2024-11-05 13:34:33 -0500

    Add player rotation left and right

--- main.py
+++ main.py
@@ -14,13 +14,18 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+        
         screen.fill((0,0,0))
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
+        
 
 
 if __name__ == "__main__":
